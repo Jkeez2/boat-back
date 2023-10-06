2ef9eaf6a9f6e9719ee26d7e57b403a0547728c7
Cebrail Yalcin <cebrail.yalcin.sch@gmail.com>
2023-10-06 10:25:03 +0200

    init project

---
+++ src/main/java/com/jkeez/boatback/BoatBackApplication.java
@@ -0,0 +1,13 @@
+package com.jkeez.boatback;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BoatBackApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(BoatBackApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/jkeez/boatback/BoatBackApplicationTests.java
@@ -0,0 +1,13 @@
+package com.jkeez.boatback;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BoatBackApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
